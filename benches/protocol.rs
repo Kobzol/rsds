2103f26383ad333d31486b80fab887b94a94fdc4
Jakub Beránek <jakub.beranek@vsb.cz>
2020-03-11 13:33:19 +0100

    ENH: add decode benchmark

--- benches/protocol.rs
+++ benches/protocol.rs
@@ -1,17 +1,75 @@
 use bytes::BytesMut;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use futures::SinkExt;
+use futures::{SinkExt, StreamExt};
 
-use rsds::protocol::protocol::{asyncwrite_to_sink, DaskPacket};
+use rsds::protocol::protocol::{asyncwrite_to_sink, split_packet_into_parts, DaskCodec, DaskPacket, Frame, asyncread_to_stream};
 use std::fs::OpenOptions;
+use std::io::Write;
 use std::time::Duration;
 use tokio::fs::File;
 use tokio::runtime;
+use tokio_util::codec::Encoder;
+use tempfile::NamedTempFile;
 
 fn create_bytes(size: usize) -> BytesMut {
     BytesMut::from(vec![0u8; size].as_slice())
 }
 
+fn serialize_packet(packet: DaskPacket) -> BytesMut {
+    let mut bytes = BytesMut::default();
+    let mut codec = DaskCodec::default();
+    for part in split_packet_into_parts(packet, 64 * 1024) {
+        codec.encode(part, &mut bytes).unwrap();
+    }
+    bytes
+}
+
+fn decode(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Decode");
+    group.warm_up_time(Duration::from_secs(1));
+    group.sample_size(10);
+
+    let sizes = vec![
+        256,
+        1024,
+        8 * 1024,
+        64 * 1024,
+        128 * 1024,
+        1024 * 1024,
+        32 * 1024 * 1024,
+        256 * 1024 * 1024,
+    ];
+    for size in sizes {
+        group.throughput(Throughput::Bytes(size as u64));
+        group.bench_with_input(BenchmarkId::new("Stream", size), &size, |b, &size| {
+            let mut rt = runtime::Builder::new()
+                .basic_scheduler()
+                .enable_io()
+                .build()
+                .unwrap();
+
+            let mut packet_file = NamedTempFile::new().unwrap();
+            let bytes = serialize_packet(DaskPacket::new(
+                Frame::from(vec![0u8; size].as_slice()),
+                vec![],
+            ));
+            packet_file.write(&bytes).unwrap();
+
+            b.iter_with_setup(
+                || {
+                    let file = File::from_std(packet_file.reopen().unwrap());
+                    asyncread_to_stream(file)
+                },
+                |mut stream| {
+                    rt.block_on(async move {
+                        stream.next().await.unwrap().unwrap();
+                    });
+                },
+            );
+        });
+    }
+    group.finish();
+}
 fn encode(c: &mut Criterion) {
     let mut group = c.benchmark_group("Encode");
     group.warm_up_time(Duration::from_secs(1));
@@ -29,26 +87,26 @@ fn encode(c: &mut Criterion) {
     ];
     for size in sizes {
         group.throughput(Throughput::Bytes(size as u64));
-        // group.bench_with_input(BenchmarkId::new("Inmemory", size), &size, |b, &size| {
-        //     b.iter_with_setup(
-        //         || {
-        //             let partsize = size / 4;
-        //             let packet = DaskPacket::new(
-        //                 create_bytes(partsize),
-        //                 (0..3).map(|_| create_bytes(partsize)).collect(),
-        //             );
-        //             packet
-        //         },
-        //         |packet| {
-        //             let mut target = BytesMut::default();
-        //             let mut codec = DaskCodec::default();
-        //             let parts = split_packet_into_parts(packet, 64 * 1024);
-        //             for part in parts {
-        //                 codec.encode(part, &mut target).unwrap();
-        //             }
-        //         },
-        //     );
-        // });
+        group.bench_with_input(BenchmarkId::new("Inmemory", size), &size, |b, &size| {
+            b.iter_with_setup(
+                || {
+                    let partsize = size / 4;
+                    let packet = DaskPacket::new(
+                        create_bytes(partsize),
+                        (0..3).map(|_| create_bytes(partsize)).collect(),
+                    );
+                    packet
+                },
+                |packet| {
+                    let mut target = BytesMut::default();
+                    let mut codec = DaskCodec::default();
+                    let parts = split_packet_into_parts(packet, 64 * 1024);
+                    for part in parts {
+                        codec.encode(part, &mut target).unwrap();
+                    }
+                },
+            );
+        });
         group.bench_with_input(BenchmarkId::new("Sink", size), &size, |b, &size| {
             let mut rt = runtime::Builder::new()
                 .basic_scheduler()
@@ -76,5 +134,5 @@ fn encode(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(protocol, encode);
+criterion_group!(protocol, encode, decode);
 criterion_main!(protocol);
